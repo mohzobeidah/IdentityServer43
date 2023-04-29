4359bf791b21892a50575428cdcf1743abfa4f4c
Mohammed <mohzobeidah@gmail.com>
2023-04-29 14:55:37 +0300

    tesf

--- Client/Controllers/WeatherForecastController.cs
+++ Client/Controllers/WeatherForecastController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Client.Controllers;
-
+//ddddddddddddddddddddddddddd
 [ApiController]
 [Route("[controller]")]
 [Authorize]
